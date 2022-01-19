041598239191304dc4df7b3bb8b2f37343527cc9
nguyendolar <nguyencaonguyencmu@gmail.com>
2022-01-19 20:37:26 +0700

    update

--- spring_workcv.sql
+++ spring_workcv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 20, 2021 lúc 11:57 AM
+-- Thời gian đã tạo: Th1 05, 2022 lúc 03:29 PM
 -- Phiên bản máy phục vụ: 10.4.11-MariaDB
 -- Phiên bản PHP: 7.2.31
 
@@ -31,15 +31,18 @@ CREATE TABLE `applypost` (
   `id` int(11) NOT NULL,
   `created_at` varchar(255) DEFAULT NULL,
   `recruitment_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL
+  `user_id` int(11) DEFAULT NULL,
+  `name_cv` varchar(255) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
 -- Đang đổ dữ liệu cho bảng `applypost`
 --
 
-INSERT INTO `applypost` (`id`, `created_at`, `recruitment_id`, `user_id`) VALUES
-(1, NULL, 3, 1);
+INSERT INTO `applypost` (`id`, `created_at`, `recruitment_id`, `user_id`, `name_cv`, `status`, `text`) VALUES
+(3, '2022-01-03', 8, 11, '1641186479919yêu-cầu-chức-năng.pdf', 1, '\n                   aaaaaaaaaaaaaaaaaaaa                                 ');
 
 -- --------------------------------------------------------
 
@@ -60,7 +63,7 @@ CREATE TABLE `category` (
 INSERT INTO `category` (`id`, `name`, `number_choose`) VALUES
 (1, 'PHP', 2),
 (2, 'JAVA', 1),
-(3, 'ASP .NET', 0),
+(3, 'ASP .NET', 1),
 (4, 'NODEJS', 3);
 
 -- --------------------------------------------------------
@@ -72,7 +75,7 @@ INSERT INTO `category` (`id`, `name`, `number_choose`) VALUES
 CREATE TABLE `company` (
   `id` int(11) NOT NULL,
   `address` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `logo` varchar(255) DEFAULT NULL,
   `name_company` varchar(255) DEFAULT NULL,
@@ -86,14 +89,15 @@ CREATE TABLE `company` (
 --
 
 INSERT INTO `company` (`id`, `address`, `description`, `email`, `logo`, `name_company`, `phone_number`, `status`, `user_id`) VALUES
-(1, 'Đà Nẵng', '<p><strong>hello</strong></p>', 'tuyendungnhansu@gmail.com', '1637145840379fpt-601x400.png', 'FPT Software', '0394073712', 0, 2),
-(3, 'Đà Nẵng', '<p><strong>hello</strong></p>', 'kmstech@gmail.com', '1637147685714getarticleimage.png', 'KMS Technology', '0394073712', 0, 5),
+(1, 'Đà Nẵng', '<p>Tham gia dự án Logistic ứng viên nhận SIGNING BONUS iPhone 12 pro max<br><br>&nbsp;<br><br>Chia sẻ ngay tới ứng viên của bạn: 5 lý do KHÔNG NÊN BỎ QUA cơ hội làm việc với Digital platforms hàng đầu thế giới!!<br><br>1. KHÁCH HÀNG WORLD-CLASS - ĐỨNG TOP THỊ TRƯỜNG TOÀN CẦU VỀ LOGISTICS<br><br>Là tập đoàn lớn nhất thế giới trong các lĩnh vực Logistics, Supply Chain, và E-commerce solutions với doanh thu 76 tỉ USD, có 540.000 nhân viên hoạt động toàn cầu, khách hàng lần này của FSOFT là công ty Logistics có hàng tỉ người dùng, có mặt trên hầu hết các nước trên thế giới.<br><br>2. ĐỐI TÁC CHUYÊN NGHIỆP, SẢN PHẨM VƯỢT TRỘI<br><br>Tham gia dự án này, các bạn sẽ được làm việc với đơn vị phụ trách phát triển các sản phẩm Digital Platformsquy mô toàn cầu tập trung vào 7 lĩnh vực công nghệ: Intelligent Automation (RPA), IoT, API, Data Lake và Data Analytics, Virtual Assistants, và Blockchain. Các sản phẩm phục vụ quy mô toàn cầu đã có từ 4 triệu đến 3 trăm triệu lượt người sử dụng hàng tháng, là ước mơ của ngành Logistics toàn thế giới. Các bạn sẽ có cơ hội làm việc với những sản phẩm digital ở mức độ chuyên nghiệp, hoàn thiện production chứ không còn ở mức thử nghiệm.</p>', 'tuyendungnhansu@gmail.com', '1640801424538download.png', 'FPT Software', '0394073712', 0, 2),
+(3, 'Đà Nẵng', '<p><strong>hello</strong></p>', 'kmstech@gmail.com', '1641185162465kms-technology.png', 'KMS Technology', '0394073712', 0, 5),
 (4, NULL, NULL, NULL, NULL, NULL, NULL, 0, 6),
 (5, NULL, NULL, NULL, NULL, NULL, NULL, 0, 7),
 (6, NULL, NULL, NULL, NULL, NULL, NULL, 0, 8),
 (7, NULL, NULL, NULL, NULL, NULL, NULL, 0, 9),
 (8, NULL, NULL, NULL, NULL, NULL, NULL, 0, 10),
-(9, NULL, NULL, NULL, NULL, NULL, NULL, 0, 11);
+(9, NULL, NULL, NULL, NULL, NULL, NULL, 0, 11),
+(10, 'Đà Nẵng', '<p>aaaaaaaaaaaaaaaaaa</p>', 'suntech@gmail.com', '1641186071267kms-technology.png', 'Sun Tech', '0394213567', 0, 12);
 
 -- --------------------------------------------------------
 
@@ -116,7 +120,7 @@ INSERT INTO `cv` (`id`, `file_name`, `user_id`) VALUES
 (3, '1637307842723bt-3.pdf', 8),
 (4, '1637308768465bt-3.pdf', 9),
 (5, '1637309030813bt-3.pdf', 10),
-(6, '1637309724722bt-3.pdf', 11);
+(10, '1641187828899nhanm2.pdf', 11);
 
 -- --------------------------------------------------------
 
@@ -129,6 +133,13 @@ CREATE TABLE `follow_company` (
   `company_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `follow_company`
+--
+
+INSERT INTO `follow_company` (`id`, `company_id`, `user_id`) VALUES
+(1, 1, 11);
 
 -- --------------------------------------------------------
 
@@ -159,11 +170,12 @@ CREATE TABLE `recruitment` (
 --
 
 INSERT INTO `recruitment` (`id`, `address`, `created_at`, `description`, `experience`, `quantity`, `rank`, `salary`, `status`, `title`, `type`, `view`, `category_id`, `company_id`, `deadline`) VALUES
-(1, 'Tam tiến Núi thành Quảng Nam', '2021-11-14', '<p>Làm việc ngày 8 tiếng tại văn phòng</p>', '3 năm kinh nghiệm', 15, NULL, '6.000.000 - 10.000.000 vnđ', 1, 'Tuyển Senior', 'Full time', 0, 1, 1, NULL),
-(3, 'Tam tiến Núi thành Quảng Nam', '2021-11-16', '<p>Hiểu rõ về vòng đời</p>', '1 năm', 13, NULL, '6.000.000 - 10.000.000 vnđ', 1, 'Tuyển lập trình react js', 'Part time', 0, 1, 1, NULL),
-(4, 'Tam tiến Núi thành Quảng Nam', '2021-11-17', '<p><strong>fsadfasdf</strong></p>', '4 năm kinh nghiệm', 12, NULL, '6.000.000 - 10.000.000 vnđ', 0, 'Tuyển Senior PHP', 'Full time', 0, 3, 1, '2021-11-20'),
-(5, 'Hải Châu, Đà Nẵng', '2021-11-17', '<p><strong>Xin chào</strong></p>', '4 năm kinh nghiệm', 12, NULL, '6.000.000 - 10.000.000 vnđ', 1, 'Tuyển Java', 'Full time', 0, 4, 3, NULL),
-(6, 'Đà Nẵng', '2021-11-17', '<p>.NET &gt; 2 năm kinh nghiệm</p>', '2 năm', 12, NULL, '12 triệu', 1, 'Tuyển lập trình viên .NET', 'Full time', 0, 2, 3, '2021-11-30');
+(1, 'Tam tiến Núi thành Quảng Nam', '2021-11-14', '<p>Làm việc ngày 8 tiếng tại văn phòng</p>', '3 năm kinh nghiệm', 15, NULL, '6.000.000 - 10.000.000 vnđ', 1, 'Tuyển Senior', 'Full time', 0, 1, 1, '2022-01-11'),
+(3, 'Tam tiến Núi thành Quảng Nam', '2021-11-16', '<p>Hiểu rõ về vòng đời</p>', '1 năm', 13, NULL, '6.000.000 - 10.000.000 vnđ', 1, 'Tuyển lập trình react js', 'Part time', 0, 1, 1, '2022-01-11'),
+(4, 'Tam tiến Núi thành Quảng Nam', '2021-11-17', '<p><strong>fsadfasdf</strong></p>', '4 năm kinh nghiệm', 12, NULL, '6.000.000 - 10.000.000 vnđ', 0, 'Tuyển Senior PHP', 'Full time', 0, 3, 1, '2022-01-03'),
+(5, 'Hải Châu, Đà Nẵng', '2021-11-17', '<p><strong>Xin chào</strong></p>', '4 năm kinh nghiệm', 12, NULL, '6.000.000 - 10.000.000 vnđ', 1, 'Tuyển Java', 'Full time', 0, 4, 3, '2022-01-11'),
+(6, 'Đà Nẵng', '2021-11-17', '<p>.NET &gt; 2 năm kinh nghiệm</p>', '2 năm', 12, NULL, '12 triệu', 1, 'Tuyển lập trình viên .NET', 'Full time', 0, 2, 3, '2021-11-30'),
+(8, 'Đà Nẵng', '2022-01-03', '<p>aaaaaaaaaaaaaaaa</p>', '2 năm', 10, NULL, '12 triệu', 1, 'Tuyển ASP.NET', 'Full time', 0, 3, 10, '2022-01-11');
 
 -- --------------------------------------------------------
 
@@ -201,7 +213,10 @@ CREATE TABLE `save_job` (
 --
 
 INSERT INTO `save_job` (`id`, `recruitment_id`, `user_id`) VALUES
-(1, 4, 1);
+(1, 4, 1),
+(2, 1, 3),
+(3, 8, 11),
+(4, 4, 11);
 
 -- --------------------------------------------------------
 
@@ -219,25 +234,27 @@ CREATE TABLE `user` (
   `password` varchar(128) NOT NULL,
   `phone_number` varchar(255) DEFAULT NULL,
   `status` int(11) DEFAULT NULL,
-  `role_id` int(11) DEFAULT NULL
+  `role_id` int(11) DEFAULT NULL,
+  `cv_id` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
 -- Đang đổ dữ liệu cho bảng `user`
 --
 
-INSERT INTO `user` (`id`, `address`, `description`, `email`, `full_name`, `image`, `password`, `phone_number`, `status`, `role_id`) VALUES
-(1, 'Tam tiến Núi thành Quảng Nam', '<p><strong>Minh nha</strong></p>', 'minhnha2308@gmail.com', 'Nguyên Minh Nhã', 'https://res.cloudinary.com/workcv/image/upload/v1637047317/wyveqapjisxapzudrhni.jpg', '25f9e794323b453885f5181f1b624d0b', '01223415449', 1, 1),
-(2, 'Tam tiến Núi thành Quảng Nam', '<p>Helo anh em</p>', 'congty@gmail.com', 'Fpt Soft', 'https://res.cloudinary.com/workcv/image/upload/v1637083609/ince0ojpgsqfkuwrg9yy.jpg', '25f9e794323b453885f5181f1b624d0b', '01223415449', 1, 2),
-(3, 'Tam tiến Núi thành Quảng Nam', '<p><strong>Hoàng</strong></p>', 'congty1@gmail.com', 'Nguyên Minh Nhã', NULL, '25f9e794323b453885f5181f1b624d0b', '01223415449', 1, 1),
-(4, NULL, NULL, 'n2ftechnology@gmail.com', 'N2F Technology', NULL, '25f9e794323b453885f5181f1b624d0b', NULL, 1, 2),
-(5, 'Tam tiến Núi thành Quảng Nam', '<p>Helo anh em</p>', 'kms@gmail.com', 'KMS', 'https://res.cloudinary.com/workcv/image/upload/v1637083609/ince0ojpgsqfkuwrg9yy.jpg', '25f9e794323b453885f5181f1b624d0b', '01223415449', 1, 2),
-(6, NULL, NULL, 'suntech@gmail.com', 'Sun Tech', NULL, '25f9e794323b453885f5181f1b624d0b', NULL, 0, 2),
-(7, NULL, NULL, 'nguyencaonguyenamv@gmail.com', 'Công ty', NULL, '25f9e794323b453885f5181f1b624d0b', NULL, 0, 2),
-(8, NULL, NULL, 'dangphat@gmail.com', 'Đăng Phát', NULL, '25f9e794323b453885f5181f1b624d0b', NULL, 1, 1),
-(9, NULL, NULL, 'hung29manh@gmail.com', 'Nguyen Manh Hung', NULL, 'e10adc3949ba59abbe56e057f20f883e', NULL, 1, 1),
-(10, NULL, NULL, 'binki56789@gmail.com', 'Đỗ Văn Tiến', NULL, '202cb962ac59075b964b07152d234b70', NULL, 1, 1),
-(11, NULL, NULL, 'quanghuy@gmail.com', 'Quang Huy', NULL, '25f9e794323b453885f5181f1b624d0b', NULL, 1, 1);
+INSERT INTO `user` (`id`, `address`, `description`, `email`, `full_name`, `image`, `password`, `phone_number`, `status`, `role_id`, `cv_id`) VALUES
+(1, 'Tam tiến Núi thành Quảng Nam', '<p><strong>Minh nha</strong></p>', 'minhnha2308@gmail.com', 'Nguyên Minh Nhã', 'https://res.cloudinary.com/workcv/image/upload/v1637047317/wyveqapjisxapzudrhni.jpg', '25f9e794323b453885f5181f1b624d0b', '01223415449', 1, 1, NULL),
+(2, 'Tam tiến Núi thành Quảng Nam', '<p>Helo anh em</p>', 'congty@gmail.com', 'Fpt Soft', 'https://res.cloudinary.com/workcv/image/upload/v1637083609/ince0ojpgsqfkuwrg9yy.jpg', '25f9e794323b453885f5181f1b624d0b', '01223415449', 1, 2, NULL),
+(3, 'Tam tiến Núi thành Quảng Nam', '<p><strong>Hoàng</strong></p>', 'congty1@gmail.com', 'Nguyên Minh Nhã', NULL, '25f9e794323b453885f5181f1b624d0b', '01223415449', 1, 1, NULL),
+(4, NULL, NULL, 'n2ftechnology@gmail.com', 'N2F Technology', NULL, '25f9e794323b453885f5181f1b624d0b', NULL, 1, 2, NULL),
+(5, 'Tam tiến Núi thành Quảng Nam', '<p>Helo anh em</p>', 'kms@gmail.com', 'KMS', 'https://res.cloudinary.com/workcv/image/upload/v1637083609/ince0ojpgsqfkuwrg9yy.jpg', '25f9e794323b453885f5181f1b624d0b', '01223415449', 1, 2, NULL),
+(6, NULL, NULL, 'suntech@gmail.com', 'Sun Tech', NULL, '25f9e794323b453885f5181f1b624d0b', NULL, 0, 2, NULL),
+(7, NULL, NULL, 'nguyencaonguyenamv@gmail.com', 'Công ty', NULL, '25f9e794323b453885f5181f1b624d0b', NULL, 0, 2, NULL),
+(8, NULL, NULL, 'dangphat@gmail.com', 'Đăng Phát', NULL, '25f9e794323b453885f5181f1b624d0b', NULL, 1, 1, NULL),
+(9, NULL, NULL, 'hung29manh@gmail.com', 'Nguyen Manh Hung', NULL, 'e10adc3949ba59abbe56e057f20f883e', NULL, 1, 1, NULL),
+(10, NULL, NULL, 'binki56789@gmail.com', 'Đỗ Văn Tiến', NULL, '202cb962ac59075b964b07152d234b70', NULL, 1, 1, NULL),
+(11, NULL, NULL, 'quanghuy@gmail.com', 'Quang Huy', NULL, '25f9e794323b453885f5181f1b624d0b', NULL, 1, 1, 10),
+(12, NULL, NULL, 'cap210diem@gmail.com', 'Bùi Văn C', 'https://res.cloudinary.com/workcv/image/upload/v1641186032/httqpeqotxpbvespjyxu.png', '25f9e794323b453885f5181f1b624d0b', NULL, 1, 2, NULL);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -306,7 +323,8 @@ ALTER TABLE `save_job`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `FKn82ha3ccdebhokx3a8fgdqeyy` (`role_id`);
+  ADD KEY `FKn82ha3ccdebhokx3a8fgdqeyy` (`role_id`),
+  ADD KEY `FK2xy5nulvty7anu7svqyskyu1r` (`cv_id`);
 
 --
 -- AUTO_INCREMENT cho các bảng đã đổ
@@ -316,7 +334,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT cho bảng `applypost`
 --
 ALTER TABLE `applypost`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT cho bảng `category`
@@ -328,25 +346,25 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT cho bảng `company`
 --
 ALTER TABLE `company`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT cho bảng `cv`
 --
 ALTER TABLE `cv`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT cho bảng `follow_company`
 --
 ALTER TABLE `follow_company`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `recruitment`
 --
 ALTER TABLE `recruitment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT cho bảng `role`
@@ -358,13 +376,13 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT cho bảng `save_job`
 --
 ALTER TABLE `save_job`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT cho bảng `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
